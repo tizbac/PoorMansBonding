ed540ba2b3e22ff55745d307f8f273a185ed2003
Tiziano Bacocco <tiziano@tizbac.ovh>
2015-06-19 23:18:42 +0200

    Added license headers

--- PoorMansBondingProtocol.py
+++ PoorMansBondingProtocol.py
@@ -1,3 +1,22 @@
+#
+# A tool designed to create a bond over TCP tunnel with crap connections ( ADSL , wimax, HSPA and stuff like that )
+# Copyright (C) 2015  Tiziano Bacocco <tiziano@tizbac.ovh>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+#
+
 from twisted.internet import reactor, protocol
 from twisted.internet.task import LoopingCall
 import time
